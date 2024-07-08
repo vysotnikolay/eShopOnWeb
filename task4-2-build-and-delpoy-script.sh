a0d1a7a1251c4d52f5bc5656c755decb2649c9ff
mikalai <vysotnikolay@gmail.com>
2024-07-08 08:56:06 +0200

    add task template and delpoyment script

--- task4-2-build-and-delpoy-script.sh
+++ task4-2-build-and-delpoy-script.sh
@@ -2,21 +2,11 @@
 # Variable block
 rootFolder=$(pwd)
 centralUsLoc="centralus"
-eastUsLoc="eastus"
 rg="CloudX"
 fileName="site.zip"
 sitePrefix="e-shop-on-web"
 
-cd src/Web
-dotnet publish -o pub
-cd pub
-zip -r $fileName *
-webPubPath=$(pwd)/$fileName
-echo "Web zip path is "$webPubPath""
-
-echo "---------------------------------------------------------------------------"
-
-cd ../../PublicApi
+cd src/PublicApi
 dotnet publish -o pub
 cd pub
 zip -r $fileName *
@@ -31,13 +21,10 @@ echo "Creating the resource group $rg"
 az group create -n $rg -l $centralUsLoc
 
 echo "Starting template deployment from $rootFolder"
-az deployment group create -n myTemplate -g $rg -f template.json
+az deployment group create -n myTemplate -g $rg -f template-web-api-and-sqls.json
 
 echo "---------------------------------------------------------------------------"
 
-
-az webapp deploy -g $rg -n $sitePrefix-front --src-path $webPubPath
-az webapp deploy -g $rg -n $sitePrefix-eastus --src-path $webPubPath
 az webapp deploy -g $rg -n $sitePrefix-api --src-path $apiPubPath
 
 read
