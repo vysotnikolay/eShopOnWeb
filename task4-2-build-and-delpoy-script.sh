0e75cc1df85535dccd40bf9023d1e254f332c771
mikalai <vysotnikolay@gmail.com>
2024-07-12 10:31:27 +0200

    script changes

--- task4-2-build-and-delpoy-script.sh
+++ task4-2-build-and-delpoy-script.sh
@@ -7,24 +7,32 @@ fileName="site.zip"
 sitePrefix="e-shop-on-web"
 
 cd src/PublicApi
+rm -rf pub
+echo "Press any key..."
+read
 dotnet publish -o pub
 cd pub
+echo "Press any key..."
+read
 zip -r $fileName *
 apiPubPath=$(pwd)/$fileName
 echo "API zip path is "$apiPubPath""
 
 echo "---------------------------------------------------------------------------"
 cd $rootFolder
+# echo "Press any key..."
+# read
+
+# echo "Creating the resource group $rg"
+# az group create -n $rg -l $centralUsLoc
+
+# echo "Starting template deployment from $rootFolder"
+# az deployment group create -n myTemplate -g $rg -f template-web-api-and-sqls.json
+
+# echo "---------------------------------------------------------------------------"
+echo "Press any key..."
 read
-
-echo "Creating the resource group $rg"
-az group create -n $rg -l $centralUsLoc
-
-echo "Starting template deployment from $rootFolder"
-az deployment group create -n myTemplate -g $rg -f template-web-api-and-sqls.json
-
-echo "---------------------------------------------------------------------------"
-
 az webapp deploy -g $rg -n $sitePrefix-api --src-path $apiPubPath
 
+echo "Press any key..."
 read
